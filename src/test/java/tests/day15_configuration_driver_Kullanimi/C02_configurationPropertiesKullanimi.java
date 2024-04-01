9111b645c875d4e18e34b7dfb13bbd5bb1228ef0
Mervan Kılıç <mervanalikilic@gmail.com>
2024-04-01 21:47:20 +0300

    15.gun sonu

--- src/test/java/tests/day15_configuration_driver_Kullanimi/C02_configurationPropertiesKullanimi.java
+++ src/test/java/tests/day15_configuration_driver_Kullanimi/C02_configurationPropertiesKullanimi.java
@@ -1,6 +1,9 @@
 package tests.day15_configuration_driver_Kullanimi;
 
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.TestOtomasyonuPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
@@ -16,12 +19,26 @@ public class C02_configurationPropertiesKullanimi {
 
         // configuration.properties'de belirlenmis olan aranacak urunu aratin
 
+        TestOtomasyonuPage testOtomasyonuPage = new TestOtomasyonuPage();
+        testOtomasyonuPage.aramaKutusu
+                .sendKeys(ConfigReader.getProperty("toAranacakKelime") + Keys.ENTER);
         // arama sonucunda urun bulunabildigini test edin
+
+        int bulunanUrunSayisi = testOtomasyonuPage.bulunanUrunElementleriList.size();
+        Assert.assertTrue(bulunanUrunSayisi > 0);
 
         // ilk urune tiklayin
 
+        testOtomasyonuPage.bulunanUrunElementleriList.get(0).click();
+
         // acilan sayfada urun isminin case sensitive olmadan
         // configuration.properties'de belirlenmis olan aranacak kelimeyi icerdigini test edin
+
+        String actualUrunIsmiKucukHarf = testOtomasyonuPage
+                                            .urunSayfasindakiUrunIsimElementi
+                                            .getText()
+                                            .toLowerCase();
+        Assert.assertTrue(actualUrunIsmiKucukHarf.contains(ConfigReader.getProperty("toAranacakKelime")));
 
         ReusableMethods.bekle(3);
         Driver.closeDriver();
